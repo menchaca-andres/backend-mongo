2d2a9b3878917dbaa9d543b892e93d0efc9e6382
4ndreees <4mrvro@gmail.com>
2025-03-13 21:58:14 -0400

    Modificacion conexion a MongoDB

--- src/config/database.js
+++ src/config/database.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB conectado");
   } catch (error) {
-    console.error("Error de conexión a MongoDB:", error);
+    console.error("Error de conexión:", error);
     process.exit(1);
   }
 };
